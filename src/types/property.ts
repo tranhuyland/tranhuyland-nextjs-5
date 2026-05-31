7145924cd9ab9b26258da14fd00585ea8027ac72
tranhuyland <huy07kt@gmail.com>
2026-05-31 08:36:36 +0700

    Update property.ts

--- src/types/property.ts
+++ src/types/property.ts
@@ -1,1 +1,23 @@
+export interface Property {
+  id: number;
+  tieude: string;
+  gia: string;
+  soGia: number;
+  khuVuc: string;
+  khuVucFull: string;
+  loaiHinh: string;
+  huong?: string;
+  dienTich: string;
+  phongNgu?: string;
+  moTa: string;
+  anh: string;
+  tag?: string;
+  tagColor?: string;
+  isMatTien?: boolean | string;
+  ngayDang?: string;
+  videoUrl?: string;
+  linkMap?: string;
+  anhSoDo?: string;
+  phapLy?: string;
+}
 
